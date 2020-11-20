e9ca9e2b3f691151e1dd8bc5468f424b57efa592
Joana-Mansa <joanaowusu279@gmail.com>
2020-11-20 15:32:25 +0000

    Flaskblueprint

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
-app= Flask(__name__)
 
-@app.route('/')
-def index():
-    return "this is an example app"
+# flasblueprint encapsulates functionality eg. views and templates
+from flask import Flask
+from example_blueprint import example_blueprint
+
+app= Flask(__name__)
+app.register_blueprint(example_blueprint)
+
+#@app.route('/')
+#def index():
+    #return "this is an example app"
